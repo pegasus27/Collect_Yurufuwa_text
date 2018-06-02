109cfb551c779f7c2df19ba3253a70cea18a6a63
pegasus27 <pega.fk.sus27@gmail.com>
2018-06-02 23:20:20 +0900

    Develop now Bug:TypeError: object of type 'Response' has no len()

--- src/Collect_Text.py
+++ src/Collect_Text.py
@@ -1,11 +1,21 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 import sys
+import requests
+from bs4 import BeautifulSoup
+#スクレイピング(抽出)に特化した機能を持っている
+
 def Collect_text():
     print("")
+    html = requests.get("https://imascg-slstage-wiki.gamerch.com/%E9%AB%98%E6%A3%AE%E8%97%8D%E5%AD%90#content_2_1")
+    soup = BeautifulSoup(html,"html.parser")
+    print(type(soup))
+    print(soup.prettify())
+
+    print("finish!!!")
 
 if __name__ == "__main__":
-    #
+    print("Collecting Text...")
     print("""
         　　　　　　　　　　 　 　 　 　 　 ＿＿＿|:./:./: |:.:|:ﾞ|ハ＼
         　　　　　　　　　　　 　 　_.　＜_.─:.:.:.:.:.:.:⌒＼）_）_）Yi＼＼
@@ -17,7 +27,7 @@ if __name__ == "__main__":
         　　　 　 　 　 　 |:.|l:.:.八: ├　　 　 >芥ｉ(卞、:.:. ＼ｰr─
         　　　 　 　 　 　 |:八:.:.:.: 芥心　　 　 弋zｿ |:.＼「ヽ: |
         　　　　　　　　　　 　＼:八 弋:〉　　 　　: : /:.:.:.:.|ノ）ﾉ
-        　　　　　 　 　 　 　 　 |＼＼　　'　　　　 /:.ｲ:.:. |＼
+       ゆるふわ～ 　　　　　 　 　 |＼＼　　'　　　　 /:.ｲ:.:. |＼
         　　　　　　　　 　 　 ﾉ:.:.: 人 　 ー‐ ′ .　八:.き )）
         　　　　　　 　 　 　 ／:.:.:.:/＿ ＞　 __. イｉｉ/:.:/）:.:.＼
         　　　　　　　　　 〃/:.:.:.:.|: //ﾞ）　ノ//｝　〈:.:〈（.:.:.:.:.:.:）＼
